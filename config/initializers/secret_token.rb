a874440328bd6c81a0c3e479ffebe888fd3cc5c3
Rob Whittaker <cyclistsdismount@gmail.com>
2013-08-14 16:47:47 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kojo::Application.config.secret_key_base = 'a89aa9254d9934ca963ef7cb0004e9963585973884711676a0027dadcb8148c93b5238dc14ec6dad530cea66386198e87e7639a1f03341b1b37dfab5dbfb9960'
---
+++ config/routes.rb
@@ -0,0 +1,2 @@
+Kojo::Application.routes.draw do
+end
